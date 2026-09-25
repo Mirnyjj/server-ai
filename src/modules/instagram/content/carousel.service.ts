486416d1ead5feb9b764bda6543cf1b32922b1a8
Максим <136368953+Mirnyjj@users.noreply.github.com>
2026-09-25 10:16:58 +0400

    fix(imports): add .js extensions

--- src/modules/instagram/content/carousel.service.ts
+++ src/modules/instagram/content/carousel.service.ts
@@ -1,6 +1,6 @@
-import { createInstagramContainerService } from "./container.service";
-import { createInstagramPublishService } from "./publish.service";
-import type { CreateCarouselInput } from "./content.types";
+import { createInstagramContainerService } from "./container.service.js";
+import { createInstagramPublishService } from "./publish.service.js";
+import type { CreateCarouselInput } from "./content.types.js";
 
 export function createInstagramCarouselService(accessToken: string) {
   const containerService = createInstagramContainerService(accessToken);
